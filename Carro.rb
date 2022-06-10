2460e15ad10c44b5acb5d481190a24e02ca47dcf
JosuePiedra <92841886+JosuePiedra@users.noreply.github.com>
2022-06-09 20:06:13 -0500

    Update Carro.rb

--- Carro.rb
+++ Carro.rb
@@ -17,7 +17,7 @@ end
 c1 = Carro.new("rojo", "Toyota", "LBB1342", 40.00)
 # Se crea un objeto c1 de tipo Carro con 4 argumentos. Si se aceptara la sobrecarga, el objeto
 # se cargaría con el primer constructor. Sin embargo, notaremos que obtenemos un error
-# al momento de compilar.
+# al momento de compilar, advirtiendo que poseemos argumentos de más
 
 
 
